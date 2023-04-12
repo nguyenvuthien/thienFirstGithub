6a6eac2c0e91574a75bb26d01581be5e35a5fd5d
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-13 02:16:46 +0700

    complete get all nodejs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,7 +17,23 @@ let handleLogin = async (req, res) => {
         user: userData.user ? userData.user : {}
     })
 }
-
+let handleGetAllUsers = async (req, res) => {
+    let id = req.body.id; //ALL, id
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters',
+            users: []
+        })
+    }
+    let users = await userService.getAllUsers(id);
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users
+    })
+}
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers
 }
