d940ae6c50851f8c50b707225340017891ab9242
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-13 20:43:12 +0700

    video 39 nodejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -46,7 +46,7 @@ let getEditCRUD = async (req, res) => {
 let putCRUD = async (req, res) => {
     let data = req.body;
     let allUser = await CRUDService.updateUserData(data);
-    return res.render('displayCRUD', {
+    return res.render('displayCRUD.ejs', {
         dataTable: allUser
     })
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,20 @@
+import { Association } from "sequelize";
 import db from "../models/index";
 import bcrypt from 'bcryptjs';
+
+const salt = bcrypt.genSaltSync(10);
+let hasUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword);
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 let handleUserLogin = (email, password) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -81,7 +96,105 @@ let getAllUsers = (userId) => {
         }
     })
 }
+
+let createNewUser = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //check email is exist
+            let check = await checkUserEmail(data.email);
+            if (check === true) {
+                resolve({
+                    errCode: 1,
+                    message: 'Your email is already in used, Please try another email!'
+                })
+            } else {
+                let hashPasswordFromCrypt = await hasUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromCrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    //image: DataTypes.STRING,
+                    roleId: data.roleId
+                    //positionId: data.positionId
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'Ok'
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+let deleteUser = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        let foundUser = await db.User.findOne({
+            where: { id: userId }
+        })
+        if (!foundUser) {
+            resolve({
+                errCode: 2,
+                errMessage: `The user isn't exist`
+            })
+        }
+
+        await db.User.destroy({
+            where: { id: userId }
+        })
+        resolve({
+            errCode: 0,
+            message: `The user is deleted`
+        })
+    })
+}
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.id) {
+                resolve({
+                    errCode: 2,
+                    errMessage: 'Missing required parameters'
+                })
+            }
+            let user = await db.User.findOne({
+                where: { id: data.id },
+                raw: false
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                await user.save();
+                // await db.User.save({
+                //     firstName: data.firstName,
+                //     lastName: data.lastName,
+                //     address: data.address
+                // })
+
+                resolve({
+                    errCode: 0,
+                    message: 'Update the user succeeds!'
+                })
+            } else {
+                resolve({
+                    errCode: 1,
+                    errMessage: `User's not found!`
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
     handleUserLogin: handleUserLogin,
-    getAllUsers: getAllUsers
+    getAllUsers: getAllUsers,
+    createNewUser: createNewUser,
+    deleteUser: deleteUser,
+    updateUserData: updateUserData
 }
