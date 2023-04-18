bc735a41677824111ae77371eb2511a5586abf76
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-18 11:44:56 +0700

    video 52 nodejs

--- src/route/web.js
+++ src/route/web.js
@@ -15,11 +15,13 @@ let initWebRoutes = (app) => {
 
     router.post('/api/login', userController.handleLogin);
     router.get('/api/get-all-users', userController.handleGetAllUsers);
-    router.post('/api/create-new-user', userController.handleCreateNewUser)
-    router.put('/api/edit-user', userController.handleEditUser)
-    router.delete('/api/delete-user', userController.handleDeleteUser)
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser);
 
-    return app.use("/", router)
+    router.get('/allcode', userController.getAllCode);
+
+    return app.use("/", router);
 }
 
 module.exports = initWebRoutes;
