19cbb5e993f504a6e6b501945d739dcc96e1a51f
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-24 13:40:33 +0700

    video 68 nodejs

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -35,12 +35,27 @@ let postInforDoctor = async (req, res) => {
         console.log(e);
         return res.status(200).json({
             errCode: -1,
-            message: 'Error from server...'
+            message: 'Error from server'
         })
     }
 }
+
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            message: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctor: getAllDoctor,
-    postInforDoctor: postInforDoctor
+    postInforDoctor: postInforDoctor,
+    getDetailDoctorById: getDetailDoctorById
 }
