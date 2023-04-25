495da9d449c0e9f9e050d9acff4eaf8a49848c6e
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-26 01:51:04 +0700

    video 76 nodejs

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -159,17 +159,9 @@ let bulkCreateSchedule = (data) => {
                     }
                 );
 
-                // convert date
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
-
                 //compare different
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
 
                 //create data
@@ -201,7 +193,12 @@ let getScheduleByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] }
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = [];
